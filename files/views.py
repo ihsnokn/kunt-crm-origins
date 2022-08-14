4ba367063d5257b33026b35f42ec76ad5762648a
ihsnokn <52039721+ihsnokn@users.noreply.github.com>
2022-08-14 14:47:05 +0300

    fırst
    first

---
+++ files/views.py
@@ -0,0 +1,165 @@
+from distutils.log import Log
+import os
+from django.shortcuts import render, redirect, reverse,get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from files.helper import classification_helper
+from .models import File, Image, Lawyer
+from .forms import FileForm, FileModelForm, CustomUserCreationForm, ImageForm
+from django.views import generic
+from django.views.generic.edit import FormView
+from django.contrib.auth import logout
+
+# CRUD create retrieve update delete + list
+SOURCE_CHOICES = (
+        # database name and display value respectively
+        ('evrak_bekleyen', 'Evrak Bekleyen'),
+        ('talep', 'Talep'),
+        ('dava_acilacak', 'Dava Açılacak'),
+        ('ara_karar', 'Ara Karar'),
+        ('bilir_kisi_raporu', 'Bilir Kişi Raporu'),
+        ('karar', 'Karar'),
+        ('icra', 'İcra'),
+        ('odeme_bekleyen', 'Ödeme Bekleyen'),
+        ('kapanis', 'Kapanış'),
+        ('ikinci_talep', '2. Talep'),
+    )
+
+
+class SignUpView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
+
+# LANDING PAGE VIEW
+class LandingPageView(generic.TemplateView):
+    template_name = "landing.html"
+
+def landing_page(request):
+    return render(request, "landing.html")
+
+# FILE LIST VIEW
+class FileListView(LoginRequiredMixin, generic.ListView):
+    template_name = "files/file_list.html"
+    queryset = File.objects.all()
+    context_object_name = "files"
+
+def file_list(request):
+
+    files = File.objects.all()
+
+    context = {
+        "files": files
+    }
+
+    return render(request, "files/file_list.html", context)
+
+# FILE DETAIL VIEW
+
+def FileDetail(request,id):
+    file = get_object_or_404(File,id = id)
+    images = file.image_set.all()
+    context = {
+        "file": file,
+        "images": images,
+    }
+    return render(request, "files/file_detail.html", context)
+
+def FileCreateView(request):
+    form=FileModelForm(request.POST or None)
+    formimage=ImageForm(request.POST or None)
+    dosya_durumları=SOURCE_CHOICES
+    lawyers=Lawyer.objects.all()   
+    if request.method == 'POST':
+        files = request.FILES.getlist("image")
+        name= form.data.get('dosya_no')
+        if not (form.errors):
+                classification_helper(name,files,form)
+               # messages.success(request,"You are added product successfully.")
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    return render(request,"files/file_create.html",{'form':form,'formimage':formimage,'dosya_durumları':dosya_durumları,"lawyers":lawyers})
+
+def sign_out(request):
+	logout(request)
+	return redirect("login") 
+
+# FILE UPDATE VIEW
+
+def FileUpdateView(request,pk):
+
+    file = get_object_or_404(File,id = pk)
+    form = FileModelForm()
+    images = file.image_set.all()
+    dosya_durumları=SOURCE_CHOICES
+    lawyers=Lawyer.objects.all()   
+    if request.method == "POST":
+        dosya_no = request.POST['dosya_no']
+        basvuran = request.POST['basvuran']
+        basvurulan = request.POST['basvurulan']
+        plaka = request.POST['plaka']
+        basvuru_konusu = request.POST['basvuru_konusu']
+        dava_tarihi = request.POST['dava_tarihi']
+        dosya_durumu = request.POST['dosya_durumu']
+        olusturan = request.POST['olusturan']
+        dosya = request.FILES['image']
+
+        a = File(dosya_no=dosya_no, basvuran=basvuran, basvurulan=basvurulan, plaka=plaka, basvuru_konusu=basvuru_konusu,
+                 dava_tarihi=dava_tarihi, dosya_durumu=dosya_durumu, olusturan=olusturan, dosya=dosya)
+        a.save()
+        return redirect("/files",{'file':file,'dosya_durumları':dosya_durumları,"lawyers":lawyers,"images": images})
+   
+    return render(request,"files/file_update.html",{'file':file,'dosya_durumları':dosya_durumları,"lawyers":lawyers,"images": images} )
+
+
+# class FileUpdateView(LoginRequiredMixin, generic.UpdateView):
+#     template_name = "files/file_update.html"
+#     queryset = File.objects.all() 
+#     form_class = FileModelForm
+#     def get_success_url(self):
+#         return reverse("files:file-list")
+
+# def file_update(request, pk):
+#     file = File.objects.get(id=pk)
+#     form = FileModelForm(instance=file)
+#     dosya_durumları=SOURCE_CHOICES
+#     lawyers=Lawyer.objects.all()   
+#     if request.method == "POST":
+#         dosya_no = request.POST['dosya_no']
+#         basvuran = request.POST['basvuran']
+#         basvurulan = request.POST['basvurulan']
+#         plaka = request.POST['plaka']
+#         basvuru_konusu = request.POST['basvuru_konusu']
+#         dava_tarihi = request.POST['dava_tarihi']
+#         dosya_durumu = request.POST['dosya_durumu']
+#         olusturan = request.POST['olusturan']
+#         dosya = request.FILE['dosya']
+
+#         a = File(dosya_no=dosya_no, basvuran=basvuran, basvurulan=basvurulan, plaka=plaka, basvuru_konusu=basvuru_konusu,
+#                  dava_tarihi=dava_tarihi, dosya_durumu=dosya_durumu, olusturan=olusturan, dosya=dosya)
+#         a.save()
+#         return redirect("/files",{'file':file,'dosya_durumları':dosya_durumları,"lawyers":lawyers})
+#     else:
+#         return redirect("/files",{'file':file,'dosya_durumları':dosya_durumları,"lawyers":lawyers} )
+
+# def handle_uploaded_file(f):
+#     with open(f.name, 'wb+') as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)
+
+# FILE DELETE VIEW
+
+class FileDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "files/file_delete.html"
+    queryset = File.objects.all()
+
+    def get_success_url(self):
+        return reverse("files:file-list")
+
+def file_delete(request, pk):
+    file = File.objects.get(id=pk)
+    file.delete()
+    return redirect("/files")
---
+++ runserver.sh
@@ -0,0 +1,9 @@
+export READ_DOT_ENV_FILE=True
+
+python manage.py collectstatic --no-input
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+gunicorn --worker-tmp-dir /tmp kuntcrm.wsgi
