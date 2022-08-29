69a7bcba8f8617e87b73a983291cfcb3e5627f5e
DjMicCheck <89160631+msozkan90@users.noreply.github.com>
2022-08-30 01:35:21 +0300

    Fix helper.py

--- files/helper.py
+++ files/helper.py
@@ -2,33 +2,27 @@ import os
 from .models import File,Image
 from django.conf import settings
 
-def classification_helper(name,files,form,lawyer):
-       
-        file = form.save(commit=False)
-        file.lawyer=lawyer
-        file.save()
-
-        file_name = File.objects.filter(id=file.id).first()
-    
+def classification_helper(files,form,lawyer):       
+        file_1 = form.save(commit=False)
+        file_1.lawyer=lawyer
+        file_1.save()
+        file_name = File.objects.filter(id=file_1.id).first()   
         for file in files:
+            if (len(file.name) > 180):               
+                file_1.delete()               
+                print(len(file.name))
+                return False
             deneme= Image.objects.create(file_name=file_name, image=file)
             deneme.save()
             initial_path = deneme.image.path
-            print(initial_path)
             directory = str(file_name.id)
-            print(directory)
             parent_dir = settings.MEDIA_ROOT +"//class" ## windows için \\ 
             path = os.path.join(parent_dir, directory)
-            print(path)
             x = str(deneme.image).split("/")
-            print(x)
             if not os.path.exists(path):
                 os.mkdir(path)
             new_path = settings.MEDIA_ROOT + "//" + x[0] + "//"  +  str(file_name.id)  + "//" + x[-1]  ## windows için \\
-            print(new_path) 
-            print(x[0] + "//"  +  str(file_name.id)  + "//" + x[-1])  ## windows için \\ 
             link_path=x[0] + "//"  +  str(file_name.id)  + "//" + x[-1]  ## windows için \\ 
-            print(link_path) 
             os.replace(initial_path, new_path)
             deneme.image = link_path 
             deneme.save()
