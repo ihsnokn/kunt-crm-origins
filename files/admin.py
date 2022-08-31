16d82149260c6c9e44002633b7a08eeb95440cae
DjMicCheck <89160631+msozkan90@users.noreply.github.com>
2022-08-31 16:30:05 +0300

    register file_notu

--- files/admin.py
+++ files/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Image, User, File, Lawyer, UserProfile
+from .models import Image, User, File, Lawyer, UserProfile,File_Notes
 
 admin.site.register(User)
 admin.site.register(UserProfile)
 admin.site.register(File)
 admin.site.register(Lawyer)
+admin.site.register(File_Notes)
 
 
 admin.site.register(Image)
-# Register your models here.
